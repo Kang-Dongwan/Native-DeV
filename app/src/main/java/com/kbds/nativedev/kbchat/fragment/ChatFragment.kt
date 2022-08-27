3d6f680b31e8c7fb4a337f41c5bcf67dd129a4ab
gyuhyeok <18centuryboy@gmail.com>
2022-08-28 08:47:54 +0900

    채팅리스트 모델 수정

--- app/src/main/java/com/kbds/nativedev/kbchat/fragment/ChatFragment.kt
+++ app/src/main/java/com/kbds/nativedev/kbchat/fragment/ChatFragment.kt
@@ -21,10 +21,19 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.kbds.nativedev.kbchat.MainActivity
 import com.kbds.nativedev.kbchat.model.ChatRoom
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 // 파이어베이스 접근하기 위한 객체 생성
 //private lateinit var database: DatabaseReference
 private val database = FirebaseDatabase.getInstance().reference
+
+
+data class ChatListModel(
+    var uid: String? = null,
+    var chatId: String? = null,
+    var chatName: String? = null
+)
 
 class ChatFragment : Fragment() {
     companion object {
@@ -54,17 +63,13 @@ class ChatFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
 
-        println("onCreateView!!!!!")
-
+        // inflater : xml로 정의된 view를 실제 객체화 시키는 용도
         val view = inflater.inflate(R.layout.fragment_chat, container, false)
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.chatFragment_recyclerview)
-        println("onCreateView > recyclerView!!!!!")
 
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = RecyclerViewAdapter()
-
-        println("onCreateView > RecyclerViewAdapter!!!!!")
 
 
         return view
@@ -77,23 +82,24 @@ class ChatFragment : Fragment() {
 
         // Firebase 데이터베이스에서 데이터를 수신하고 ArrayList를 채운다, 그리고 ArrayList가 recyclerview 어댑터의 데이터소르로 사용됩니다
         // 채팅방 리스트
-        private val chatModel = ArrayList<ChatRoom>()
+        //private val chatModel = ArrayList<ChatRoom>()
+        private val chatModel = ArrayList<ChatListModel>()
         private var uid : String? = null        // ?는 uid가 null일수도 있음을 표시
         private val destinationUsers : ArrayList<String> = arrayListOf()
 
 
         init {
-            //
+            // 사용자의 uid
             uid = Firebase.auth.currentUser?.uid.toString()
             println("uid :" + uid)
 
 
-            database.child("chatList").child("uid").child("chatId").child("chatName").get().addOnSuccessListener {
-                Log.i("firebase", "Got value ${it.value}")
-
-            }.addOnFailureListener{
-                Log.e("firebase", "Error getting data", it)
-            }
+//            database.child("chatList").child("uid").child("chatId").child("chatName").get().addOnSuccessListener {
+//                Log.i("firebase", "Got value ${it.value}")
+//
+//            }.addOnFailureListener{
+//                Log.e("firebase", "Error getting data", it)
+//            }
 
             //println("데이터 : " + database.child("chatList").child(uid!!).child("chatId").child("chatName").get().toString())
 
@@ -109,8 +115,13 @@ class ChatFragment : Fragment() {
 
                     for (data in snapshot.children){
                         println("data : " + data)
-                        chatModel.add(data.getValue<ChatRoom>()!!)
-                        println("data : " + data)
+                        var uid = data.child("uid").toString()
+                        var chatId = data.child("chatId").toString()
+                        var chatName = data.child("chatName").toString()
+
+                        val chatLstMdl = ChatListModel(uid, chatId, chatName)
+                        chatModel.add(chatLstMdl)
+                        //println("data : " + data)
                     }
                     notifyDataSetChanged()
                 }
@@ -139,6 +150,7 @@ class ChatFragment : Fragment() {
 
         // recyclerview가 viewholder를 가져와 데이터를 연결할때 호출
         // 적절한 데이터를 가져와서 그 데이터를 사용하여 뷰홀더의 레이아웃을 채움
+        // 항목뷰에 데이터를 연결
         override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
             var destinationUid: String? = null
 
@@ -146,12 +158,26 @@ class ChatFragment : Fragment() {
 
 
             //채팅방에 있는 유저 모두 체크
-            for (user in chatModel[position].uid.keys) {
+            for (user in chatModel[position].uid.toString()) {
                 if (!user.equals(uid)) {
-                    destinationUid = user
+                    destinationUid = user.toString()
                     destinationUsers.add(destinationUid)
                 }
             }
+
+
+            database.child("chatList").child(uid!!).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError) {
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+//                    val friend = snapshot.getValue<Friend>()
+//                    Glide.with(holder.itemView.context).load(friend?.profileImageUrl)
+//                        .apply(RequestOptions().circleCrop())
+//                        .into(holder.imageView)
+//                    holder.textView_title.text = friend?.name
+                }
+            })
 
             //채팅창 선택시 이동
             holder.itemView.setOnClickListener {
@@ -163,7 +189,7 @@ class ChatFragment : Fragment() {
 
         }
 
-        // 뿌려줄 데이터의 전체 길이를 리턴
+        // 뿌려줄 데이터의 전체 길이를 리턴  (아이템갯수)
         override fun getItemCount(): Int {
             return chatModel.size
         }
