af58a7fa45524f9985ce5b9b8ee20462c8555d03
phw4302 <phw4320@gmail.com>
2022-08-27 23:14:25 +0900

    친구 프로필 상세 친구삭제 버튼 기능 추가

--- app/src/main/java/com/kbds/nativedev/kbchat/MainActivity.kt
+++ app/src/main/java/com/kbds/nativedev/kbchat/MainActivity.kt
@@ -1,29 +1,20 @@
 package com.kbds.nativedev.kbchat
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.firebase.auth.FirebaseAuth
-import com.kbds.nativedev.kbchat.databinding.ActivityMainBinding
 import com.kbds.nativedev.kbchat.fragment.ChatFragment
 import com.kbds.nativedev.kbchat.fragment.FriendFragment
 import com.kbds.nativedev.kbchat.fragment.SettingFragment
-import com.kbds.nativedev.kbchat.fragment.SettingNaviFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_setting_navi.*
 
 private lateinit var friendFragment: FriendFragment
 private lateinit var chatFragmet: ChatFragment
 private lateinit var settingFragmet: SettingFragment
-private lateinit var settingNaviFragment: SettingNaviFragment
-private lateinit var nextIntent: Intent
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     //--2022.08.23 뒤로가기
     interface onBackPressedListener{
         fun onBackPressed()
@@ -36,23 +27,22 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        bottom_nav.setOnNavigationItemSelectedListener(bottomNavItemSelectedListener)
+        bottom_nav.setOnNavigationItemSelectedListener(BottomNavItemSelectedListener)
         friendFragment = FriendFragment.newInstance()
         supportFragmentManager.beginTransaction().replace(R.id.fragment_frame, friendFragment).commit()
-        nextIntent = Intent(this, LoginActivity::class.java)
     }
 
-    private lateinit var callback: OnChangeSettingFragment
+    fun setToolbar() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
-    private val bottomNavItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener{
+    private val BottomNavItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener{
         Log.d("itemId", "${it.itemId}")
         Log.d("menu_friend", "${R.id.menu_friend}")
         Log.d("menu_chat", "${R.id.menu_chat}")
         Log.d("menu_setting", "${R.id.menu_setting}")
-
         when(it.itemId){
             R.id.menu_friend -> {
                 friendFragment = FriendFragment.newInstance()
@@ -63,49 +53,10 @@ class MainActivity : AppCompatActivity() {
                 supportFragmentManager.beginTransaction().replace(R.id.fragment_frame, chatFragmet).commit()
             }
             R.id.menu_setting -> {
-                callback = object : OnChangeSettingFragment {
-                    override fun onModifyUser() {
-                        supportFragmentManager.beginTransaction().replace(R.id.fragment_frame, settingFragmet).commit()
-                    }
-                    override fun onLogOut() {
-                        //val nextIntent = Intent(this, LoginActivity::class.java)
-                        startActivity(nextIntent)
-                    }
-                }
                 settingFragmet = SettingFragment.newInstance()
-                settingNaviFragment = SettingNaviFragment.newInstance(callback)
-
-                supportFragmentManager.beginTransaction().replace(R.id.fragment_frame, settingNaviFragment).commit()
-
-
-                // 주석 처리 합니다
-//                val intentMain = Intent(this, SettingNaviActivity::class.java)
-//                startActivity(intentMain)
-/*
-                settingNaviFragment.btn_logout.setOnClickListener {
-                    val intentMain = Intent(this, SettingNaviActivity::class.java)
-                    startActivity(intentMain)
-                }*/
-/*
-                settingNaviFragment.btn_modify.setOnClickListener {
-                    //this.OnChangeSettingFragment
-                    //SettingNaviFragment.onChangeSettingFrag?.onModifyUser()
-                    //MainActivity.OnChangeSettingFragment.onModifyUser()
-                    //OnChangeSettingFragment.onModifyUser
-                    //MainActivity.OnChangeSettingFragment?.
-
-                }
-*/
-
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_frame, settingFragmet).commit()
             }
         }
         true
     }
-
-    interface OnChangeSettingFragment {
-        fun onModifyUser()
-        fun onLogOut()
-    }
-
-
 }
--- app/src/main/java/com/kbds/nativedev/kbchat/ProfileDetailActivity.kt
+++ app/src/main/java/com/kbds/nativedev/kbchat/ProfileDetailActivity.kt
@@ -1,24 +1,39 @@
 package com.kbds.nativedev.kbchat
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.kbds.nativedev.kbchat.adapters.ListAdapter
 import com.kbds.nativedev.kbchat.fragment.TestData
 import kotlinx.android.synthetic.main.activity_profile_detail.*
 import kotlinx.android.synthetic.main.fragment_friend.*
 
 class ProfileDetailActivity : AppCompatActivity() {
-    private val imageView: Any
-        get() {
-            TODO()
-        }
+
+    private lateinit var deleteFriendBtn: Button
+    val database = Firebase.database
+    val myRef = database.getReference("friend")
+    val databaseIns = FirebaseDatabase.getInstance().reference
+    val user = auth.currentUser
+
+
     private lateinit var listAdapter: ListAdapter
     var dataList: ArrayList<TestData> = arrayListOf(
     )
@@ -27,8 +42,9 @@ class ProfileDetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile_detail)
         dataList.clear();
         var list = dataList
-
         var imageView = R.id.profileImage
+        deleteFriendBtn = findViewById(R.id.deleteFriendBtn)
+
 
         val name = intent.getStringExtra("name")
         val comment = intent.getStringExtra("comment")
@@ -43,6 +59,57 @@ class ProfileDetailActivity : AppCompatActivity() {
             .error(R.drawable.user) // 이미지로드 실패시 로컬 user.png
             .circleCrop()
             .into(profileImage)
+
+        deleteFriendBtn.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("친구삭제")
+                .setMessage("현재 친구를 삭제하시겠습니까?")
+                .setPositiveButton("확인",
+                DialogInterface.OnClickListener{ dialog, id ->
+                    FirebaseDatabase.getInstance().reference.child("user").addValueEventListener(object :
+                        ValueEventListener {
+                        override fun onDataChange(dataSnapshot: DataSnapshot) {
+                            for (snapshot in dataSnapshot.children) {
+                                var friendUid = snapshot.key
+                                var name1 = snapshot.child("name")
+                                var comment = snapshot.child("comment")
+                                var freindUid02 = snapshot.child("uid")
+
+                                Log.d("test", "test000 = "+ name)   //db
+                                //Log.d("test", "test111 = "+ userName) //input
+                                Log.d("test", "test222 = "+ freindUid02) //input
+                                if(name1.value?.equals(name) == true) {
+                                    Log.d("test", "test123")
+                                    user?.let {
+                                        Log.d("FirstFragment", "deleteBtn:userUid:" + user.uid)
+                                        myRef.child(user.uid).child(friendUid.toString()).removeValue()
+                                        Toast.makeText(this@ProfileDetailActivity, "친구삭제 성공", Toast.LENGTH_LONG).show()
+                                        val nextIntent = Intent(this@ProfileDetailActivity, MainActivity::class.java)
+                                        startActivity(nextIntent)
+                                        return
+                                    }
+                                }
+
+                                //val map = snapshot.getValue(Map::class.java) as Map<String, String>
+                                //val comment = map.get("comment").toString()
+                                //val name = map.get("name").toString()
+                                Log.d("FirstFragment", "ValueEventListener : " + snapshot.value + " a = " + name + "a.value = "+ comment)
+                            }
+                            Toast.makeText(this@ProfileDetailActivity, "친구삭제 실패", Toast.LENGTH_LONG).show()
+                        }
+
+                        override fun onCancelled(databaseError: DatabaseError) {}
+                    })
+                    //Toast.makeText(this, "친구삭제가 완료돠었습니다.", Toast.LENGTH_SHORT)
+                    //    .show()
+                })
+                .setNegativeButton("취소",
+                    DialogInterface.OnClickListener{ dialog, id ->
+                        Toast.makeText(this, "친구삭제에 실패하였습니다.", Toast.LENGTH_SHORT)
+                            .show()
+                    })
+            builder.show()
+        }
     }
 
 }
