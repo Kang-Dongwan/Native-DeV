a670cac69aaf3a76069f7c4c040e28c966f74e48
phw4302 <phw4320@gmail.com>
2022-09-02 00:04:35 +0900

    친구추가 레이아웃 변경 및 머지 누락부분 추가

--- app/src/main/java/com/kbds/nativedev/kbchat/ProfileDetailActivity.kt
+++ app/src/main/java/com/kbds/nativedev/kbchat/ProfileDetailActivity.kt
@@ -2,18 +2,16 @@ package com.kbds.nativedev.kbchat
 
 import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import com.bumptech.glide.Glide
+import com.google.android.material.internal.ContextUtils.getActivity
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -21,6 +19,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.kbds.nativedev.kbchat.adapters.ListAdapter
+import com.kbds.nativedev.kbchat.fragment.ChatFragment
 import com.kbds.nativedev.kbchat.fragment.TestData
 import kotlinx.android.synthetic.main.activity_profile_detail.*
 import kotlinx.android.synthetic.main.fragment_friend.*
@@ -28,9 +27,11 @@ import kotlinx.android.synthetic.main.fragment_friend.*
 class ProfileDetailActivity : AppCompatActivity() {
 
     private lateinit var deleteFriendBtn: Button
+    private lateinit var chatBtn: Button
     val database = Firebase.database
     val myRef = database.getReference("friend")
     val databaseIns = FirebaseDatabase.getInstance().reference
+    val auth = Firebase.auth
     val user = auth.currentUser
 
 
@@ -44,6 +45,7 @@ class ProfileDetailActivity : AppCompatActivity() {
         var list = dataList
         var imageView = R.id.profileImage
         deleteFriendBtn = findViewById(R.id.deleteFriendBtn)
+        chatBtn = findViewById(R.id.chatBtn)
 
 
         val name = intent.getStringExtra("name")
@@ -65,50 +67,66 @@ class ProfileDetailActivity : AppCompatActivity() {
             builder.setTitle("친구삭제")
                 .setMessage("현재 친구를 삭제하시겠습니까?")
                 .setPositiveButton("확인",
-                DialogInterface.OnClickListener{ dialog, id ->
-                    FirebaseDatabase.getInstance().reference.child("user").addValueEventListener(object :
-                        ValueEventListener {
-                        override fun onDataChange(dataSnapshot: DataSnapshot) {
-                            for (snapshot in dataSnapshot.children) {
-                                var friendUid = snapshot.key
-                                var name1 = snapshot.child("name")
-                                var comment = snapshot.child("comment")
-                                var freindUid02 = snapshot.child("uid")
+                    DialogInterface.OnClickListener{ dialog, id ->
+                        FirebaseDatabase.getInstance().reference.child("user").addValueEventListener(object :
+                            ValueEventListener {
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                for (snapshot in dataSnapshot.children) {
+                                    var friendUid = snapshot.key
+                                    var email = snapshot.child("email")
+                                    var comment = snapshot.child("comment")
+                                    var freindUid02 = snapshot.child("uid")
 
-                                Log.d("test", "test000 = "+ name)   //db
-                                //Log.d("test", "test111 = "+ userName) //input
-                                Log.d("test", "test222 = "+ freindUid02) //input
-                                if(name1.value?.equals(name) == true) {
-                                    Log.d("test", "test123")
-                                    user?.let {
-                                        Log.d("FirstFragment", "deleteBtn:userUid:" + user.uid)
-                                        myRef.child(user.uid).child(friendUid.toString()).removeValue()
-                                        Toast.makeText(this@ProfileDetailActivity, "친구삭제 성공", Toast.LENGTH_LONG).show()
-                                        val nextIntent = Intent(this@ProfileDetailActivity, MainActivity::class.java)
-                                        startActivity(nextIntent)
-                                        return
+                                    Log.d("test", "test000 = "+ name)   //db
+                                    //Log.d("test", "test111 = "+ userName) //input
+                                    Log.d("test", "test222 = "+ freindUid02) //input
+                                    if(email.value?.equals(name) == true) {
+                                        Log.d("test", "test123")
+                                        user?.let {
+                                            Log.d("FirstFragment", "deleteBtn:userUid:" + user.uid)
+                                            myRef.child(user.uid).child(friendUid.toString()).removeValue()
+                                            Toast.makeText(this@ProfileDetailActivity, "친구삭제 성공", Toast.LENGTH_LONG).show()
+                                            val nextIntent = Intent(this@ProfileDetailActivity, MainActivity::class.java)
+                                            startActivity(nextIntent)
+                                            return
+                                        }
                                     }
+
+                                    //val map = snapshot.getValue(Map::class.java) as Map<String, String>
+                                    //val comment = map.get("comment").toString()
+                                    //val name = map.get("name").toString()
+                                    Log.d("FirstFragment", "ValueEventListener : " + snapshot.value + " a = " + name + "a.value = "+ comment)
                                 }
-
-                                //val map = snapshot.getValue(Map::class.java) as Map<String, String>
-                                //val comment = map.get("comment").toString()
-                                //val name = map.get("name").toString()
-                                Log.d("FirstFragment", "ValueEventListener : " + snapshot.value + " a = " + name + "a.value = "+ comment)
+                                Toast.makeText(this@ProfileDetailActivity, "친구삭제 실패", Toast.LENGTH_LONG).show()
                             }
-                            Toast.makeText(this@ProfileDetailActivity, "친구삭제 실패", Toast.LENGTH_LONG).show()
-                        }
 
-                        override fun onCancelled(databaseError: DatabaseError) {}
+                            override fun onCancelled(databaseError: DatabaseError) {}
+                        })
+                        //Toast.makeText(this, "친구삭제가 완료돠었습니다.", Toast.LENGTH_SHORT)
+                        //    .show()
                     })
-                    //Toast.makeText(this, "친구삭제가 완료돠었습니다.", Toast.LENGTH_SHORT)
-                    //    .show()
-                })
                 .setNegativeButton("취소",
                     DialogInterface.OnClickListener{ dialog, id ->
                         Toast.makeText(this, "친구삭제에 실패하였습니다.", Toast.LENGTH_SHORT)
                             .show()
                     })
             builder.show()
+        }
+        chatBtn.setOnClickListener {
+
+            val mFragmentManager = supportFragmentManager
+
+            val mFragmentTransaction = mFragmentManager.beginTransaction()
+            val mFragment = ChatFragment()
+
+            // On button click, a bundle is initialized and the
+            // text from the EditText is passed in the custom
+            // fragment using this bundle\
+             /*   val mBundle = Bundle()
+                mBundle.putString("friendUid",friendUid.toString())
+                mFragment.arguments = mBundle
+                mFragmentTransaction.add(R.id.container, mFragment).commit()*/
+
         }
     }
 
