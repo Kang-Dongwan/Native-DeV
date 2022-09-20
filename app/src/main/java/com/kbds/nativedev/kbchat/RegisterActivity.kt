269fee5f472a9d2f934a30a32062403538feb304
yoonmikyeung <mygod2613@naver.com>
2022-09-20 23:25:33 +0900

    [feat] 회원가입, 정보 변경 화면 신규 개발

--- app/src/main/java/com/kbds/nativedev/kbchat/RegisterActivity.kt
+++ app/src/main/java/com/kbds/nativedev/kbchat/RegisterActivity.kt
@@ -1,20 +1,108 @@
 package com.kbds.nativedev.kbchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.kbds.nativedev.kbchat.databinding.ActivityRegisterBinding
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.DatabaseReference
+import com.kbds.nativedev.kbchat.fragment.SettingFragment
+import kotlinx.android.synthetic.main.fragment_setting.*
+import android.net.Uri
+import com.google.firebase.storage.FirebaseStorage
+import com.bumptech.glide.Glide
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AlertDialog
+import android.widget.ImageView
 
 private lateinit var auth: FirebaseAuth
 lateinit var database: DatabaseReference
 
+data class UserInfo(
+    val email : String? = null,
+    val name : String? = null,
+    val uid : String? = null)
+
+data class UserImageInfo(
+    val email : String? = null,
+    val name : String? = null,
+    val profileImageUrl : String? = null,
+    val uid : String? = null)
+
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private var imageUri : Uri? = null
+
+    private fun navigateGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        /* 갤러리에서 이미지를 선택한 후, 프로필 이미지뷰를 수정하기 위해 갤러리에서 수행한 값을 받아오는 startActivityForeResult를 사용한다.*/
+        startActivityForResult(intent, 2000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // 예외처리
+        if (resultCode != RESULT_OK)
+            return
+
+        when (requestCode) {
+            // 2000: 이미지 컨텐츠를 가져오는 액티비티를 수행한 후 실행되는 Activity 일 때만 수행하기 위해서
+            2000 -> {
+                val selectedImageUri: Uri? = data?.data
+                if (selectedImageUri != null) {
+                    imageUri = selectedImageUri
+                    binding.etImageview.setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("프로필 이미지를 바꾸기 위해서는 갤러리 접근 권한이 필요합니다.")
+            .setPositiveButton("동의하기") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+            }
+            .setNegativeButton("취소하기") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    // 권한 요청 승인 이후 실행되는 함수
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    navigateGallery()
+                else
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                //
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
@@ -22,13 +110,44 @@ class RegisterActivity : AppCompatActivity() {
         auth = Firebase.auth
         database = Firebase.database.reference
 
+        var profileCheck = false
+
+
 //        val email = findViewById<EditText>(R.id.email)
 //        val pwd = findViewById<EditText>(R.id.et_pwd)
 //        val pwdAgain = findViewById<EditText>(R.id.et_re_pwd)
 //        val btnRegi = findViewById<Button>(R.id.btn_regi)
 
+        //프로필사진 바꾸기
+        binding.etImageview.setOnClickListener{
+            when {
+                // 갤러리 접근 권한이 있는 경우
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+                -> {
+                    navigateGallery()
+                    profileCheck = true
+                }
+
+                // 갤러리 접근 권한이 없는 경우 & 교육용 팝업을 보여줘야 하는 경우
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                -> {
+                    showPermissionContextPopup()
+                }
+
+                // 권한 요청 하기(requestPermissions) -> 갤러리 접근(onRequestPermissionResult)
+                else -> requestPermissions(
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1000
+                )
+            }
+
+        }
+
         binding.btnRegi.setOnClickListener {
-            if(binding.email.text.isEmpty() || binding.etPwd.text.isEmpty() ){
+            if(binding.email.text.isEmpty() || binding.etPwd.text.isEmpty()){
                 Toast.makeText(this, "아이디와 비밀번호를 입력해 주세요.", Toast.LENGTH_SHORT).show()
             } else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(binding.email.text).matches()) {
                 Toast.makeText(this, "아이디를 이메일 형식으로 입력해 주세요", Toast.LENGTH_SHORT).show()
@@ -36,27 +155,82 @@ class RegisterActivity : AppCompatActivity() {
                 Toast.makeText(this, "비밀번호는 6자리 이상 입력해 주세요", Toast.LENGTH_SHORT).show()
             } else if(binding.etPwd.text.toString() != binding.etRePwd.text.toString()) {
                 Toast.makeText(this, "비밀번호가 다릅니다.", Toast.LENGTH_SHORT).show()
+            } else if(binding.etTextviewName.text.isEmpty()){
+                Toast.makeText(this, "이름을 입력해 주세요.", Toast.LENGTH_SHORT).show()
             } else {
-                registerUser(binding.email.text.toString(), binding.etPwd.text.toString())
+                Log.d("profileCheck", "$profileCheck")
+                Log.d("email", "$binding.email.text")
+                if(!profileCheck) {
+                    registerUser(
+                        binding.email.text.toString(),
+                        binding.etPwd.text.toString(),
+                        binding.etTextviewName.text.toString()
+                    )
+                }else{
+                    registerUserImage(
+                        binding.email.text.toString(),
+                        binding.etPwd.text.toString(),
+                        binding.etTextviewName.text.toString()
+                    )
+                }
             }
         }
     }
 
-    private fun registerUser(email:String, pwd: String) {
+    private fun registerUser(email:String, pwd: String, name: String) {
         auth.createUserWithEmailAndPassword(email, pwd)
             .addOnCompleteListener(this) {
-                task -> if (task.isSuccessful) {
+                    task -> if (task.isSuccessful) {
                 finish()
                 val user = Firebase.auth.currentUser
                 val userId = user?.uid
                 val userIdSt = userId.toString()
 
-                database.child("user/$userIdSt/email").setValue(email)
+                val userInfo = UserInfo(email.toString(), name.toString(), userIdSt)
+                database.child("user").child(userId.toString()).setValue(userInfo)
 
                 Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
             }
-        }
+         }
+    }
+
+    private fun registerUserImage(email:String, pwd: String, name: String) {
+        auth.createUserWithEmailAndPassword(email, pwd)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val user = Firebase.auth.currentUser
+                    val userId = user?.uid
+                    val userIdSt = userId.toString()
+                    Log.d("이미지 URI", "$imageUri")
+                    FirebaseStorage.getInstance()
+                        .reference.child("userImages").child("$userIdSt/photo").putFile(imageUri!!)
+                        .addOnSuccessListener {
+                            var userProfile: Uri? = null
+                            FirebaseStorage.getInstance().reference.child("userImages")
+                                .child("$userIdSt/photo").downloadUrl
+                                .addOnSuccessListener {
+                                    userProfile = it
+                                    Log.d("이미지 URL", "$userProfile")
+                                    val userInfo = UserImageInfo(
+                                        email.toString(),
+                                        name.toString(),
+                                        userProfile.toString(),
+                                        userId.toString()
+                                    )
+                                    database.child("user").child(userId.toString())
+                                        .setValue(userInfo)
+                                    finish()
+                                }
+                        }
+
+                    Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
+
--- app/src/main/java/com/kbds/nativedev/kbchat/fragment/SettingFragment.kt
+++ app/src/main/java/com/kbds/nativedev/kbchat/fragment/SettingFragment.kt
@@ -2,6 +2,7 @@ package com.kbds.nativedev.kbchat.fragment
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,6 +12,9 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.ktx.auth
@@ -18,12 +22,21 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.kbds.nativedev.kbchat.R
-import android.provider.MediaStore
-import android.net.Uri
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.activity.result.ActivityResult
+import kotlinx.android.synthetic.main.activity_profile_detail.*
 import kotlinx.android.synthetic.main.fragment_setting.*
+
+data class UserInfo(
+    val uid : String? = null,
+    val email : String? = null,
+    val name : String? = null,
+    val comment : String? = null)
+
+data class UserImageInfo(
+    val uid : String? = null,
+    val email : String? = null,
+    val name : String? = null,
+    val comment : String? = null,
+    val profileImageUrl : String? = null)
 
 class SettingFragment : Fragment() {
 
@@ -43,16 +56,6 @@ class SettingFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //setContentView(R.layout.fragment_setting)
-
-        //val btnlogout = findViewById<Button>(R.id.btn_logout)
-
-        //btnlogout.setOnClickListener {
-            //FirebaseAuth.getInstance().signOut()
-        //}
-
-
     }
 
     override fun onAttach(context: Context) {
@@ -63,23 +66,10 @@ class SettingFragment : Fragment() {
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if(result.resultCode == AppCompatActivity.RESULT_OK) {
                 imageUri = result.data?.data //이미지 경로 원본
+
                 profile_imageview.setImageURI(imageUri) //이미지 뷰를 바꿈
-/*
-                    //fireStorage.child("userImages/$uid/photo").delete().addOnSuccessListener {
-                        fireStorage.child("userImages/$uid/photo").putFile(imageUri!!)
-                            .addOnSuccessListener {
-                                fireStorage.child("userImages/$uid/photo").downloadUrl.addOnSuccessListener {
-                                    val photoUri: Uri = it
-                                    println("$photoUri")
-                                    fireDatabase.child("user/$uid/profileImageUrl")
-                                        .setValue(photoUri.toString())
-                                    Toast.makeText(requireContext(), "프로필사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
-                                }
-                            }
-                    //}
-*/
+
                 Log.d("이미지", "성공")
-                //Toast.makeText(requireContext(), "프로필사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
             }
             else{
                 Log.d("이미지", "실패")
@@ -105,46 +95,8 @@ class SettingFragment : Fragment() {
         tvEmail.text = auth.currentUser?.email.toString()
 
         val uid =  auth.currentUser?.uid.toString()
-        //name.text = uid
 
-        // database test
-        //val database = Firebase.database
-        //val userRef = database.getReference("User")
-        //userRef.setValue("Hello, World!")
         val database = Firebase.database.reference
-
-        //firebaseStorage 인스턴스 생성
-        //하나의 Storage와 연동되어 있는 경우, getInstance()의 파라미터는 공백으로 두어도 됨
-        //하나의 앱이 두개 이상의 Storage와 연동이 되어있 경우, 원하는 저장소의 스킴을 입력
-        //getInstance()의 파라미터는 firebase console에서 확인 가능('gs:// ... ')
-        //firebaseStorage 인스턴스 생성
-        //하나의 Storage와 연동되어 있는 경우, getInstance()의 파라미터는 공백으로 두어도 됨
-        //하나의 앱이 두개 이상의 Storage와 연동이 되어있 경우, 원하는 저장소의 스킴을 입력
-        //getInstance()의 파라미터는 firebase console에서 확인 가능('gs:// ... ')
-        val storage: FirebaseStorage = FirebaseStorage.getInstance()
-
-        //생성된 FirebaseStorage를 참조하는 storage 생성
-        //val storageRef = storage.reference
-        val storageRef = storage.getReferenceFromUrl("gs://native-dev-chat.appspot.com")
-        //Storage 내부의 images 폴더 안의 image.jpg 파일명을 가리키는 참조 생성
-
-        //Storage 내부의 images 폴더 안의 image.jpg 파일명을 가리키는 참조 생성
-        //val pathReference = storageRef.child("IMG_2412.jpg")
-/*
-        storageRef.child("userImages/${Companion.uid}/photo").downloadUrl
-            .addOnSuccessListener{
-                var imageUrl = it
-                Log.i("fireStorage", "Got value ${imageUrl}")
-                // Download directly from StorageReference using Glide
-                // (See MyAppGlideModule for Loader registration)
-                Glide.with(this /* context */)
-                    .load(it)
-                    .into(photo!!)
-            }
-            .addOnFailureListener{
-                Log.e("fireStorage", "Error getting data", it)
-            }
-*/
 
         database.child("user").child(uid).get().addOnSuccessListener {
             Log.i("firebase", "Got value ${it.value}")
@@ -156,25 +108,31 @@ class SettingFragment : Fragment() {
 
                 var imageUrl = map.get("profileImageUrl")?.toString()
                 //Log.i("imageUrl", "Got value ${imageUrl}")
+
+
                 if(imageUrl != null){
                     Glide.with(this /* context */)
                         .load(imageUrl)
                         .into(photo!!)
+
+                }else{
+                    Glide.with(this).load(R.drawable.user)
+                        .error(R.drawable.user) // 이미지로드 실패시 로컬 user.png
+                        .circleCrop()
+                        .into(photo!!)
                 }
-            }
 
-
-        }.addOnFailureListener{
-            Log.e("firebase", "Error getting data", it)
-        }
 /*
-        database.child("user").child("uid").child(uid).child("comment").get().addOnSuccessListener {
-            Log.i("firebase", "Got value ${it.value}")
-            comment.text = "${it.value}"
+                Glide.with(this).load(imageUrl)
+                    .error(R.drawable.user) // 이미지로드 실패시 로컬 user.png
+                    .circleCrop()
+                    .into(photo!!)
+*/
+            }
         }.addOnFailureListener{
             Log.e("firebase", "Error getting data", it)
         }
- */
+
         //프로필사진 바꾸기
         photo?.setOnClickListener{
             /*
@@ -192,58 +150,32 @@ class SettingFragment : Fragment() {
         }
 
         button?.setOnClickListener{
-            if(name?.text!!.isNotEmpty()) {
-                Log.d("tag", name.text.toString())
-                database.child("user/$uid/name").setValue(name.text.toString())
-                name.clearFocus()
-                Toast.makeText(requireContext(), "이름이 변경되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-            if(comment?.text!!.isNotEmpty()) {
-                database.child("user/$uid/comment").setValue(comment.text.toString())
-                comment.clearFocus()
-                Toast.makeText(requireContext(), "상태메시지가 변경되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-            if(imageUri != null){
-                fireStorage.child("userImages/${Companion.uid}/photo").putFile(imageUri!!)
-                    .addOnSuccessListener {
-                        fireStorage.child("userImages/${Companion.uid}/photo").downloadUrl.addOnSuccessListener {
-                            val photoUri: Uri = it
-                            println("$photoUri")
-                            fireDatabase.child("user/${Companion.uid}/profileImageUrl")
-                                .setValue(photoUri.toString())
-                            Toast.makeText(requireContext(), "프로필사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-            }
+
+                  if(name?.text!!.isNotEmpty()) {
+                      Log.d("tag", name.text.toString())
+                      database.child("user/$uid/name").setValue(name.text.toString())
+                      name.clearFocus()
+                      Toast.makeText(requireContext(), "이름이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+                  }
+                  if(comment?.text!!.isNotEmpty()) {
+                      database.child("user/$uid/comment").setValue(comment.text.toString())
+                      comment.clearFocus()
+                      Toast.makeText(requireContext(), "상태메시지가 변경되었습니다.", Toast.LENGTH_SHORT).show()
+                  }
+                  if(imageUri != null){
+                      fireStorage.child("userImages/${Companion.uid}/photo").putFile(imageUri!!)
+                          .addOnSuccessListener {
+                              fireStorage.child("userImages/${Companion.uid}/photo").downloadUrl.addOnSuccessListener {
+                                  val photoUri: Uri = it
+                                  println("$photoUri")
+                                  fireDatabase.child("user/${Companion.uid}/profileImageUrl")
+                                      .setValue(photoUri.toString())
+                                  Toast.makeText(requireContext(), "프로필사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+                              }
+                          }
+                  }
         }
 
-        /*
-        userRef.child("users").child(uid).addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onCancelled(error: DatabaseError) {
-            }
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val userProfile = snapshot.getValue<String>()
-
-               // email?.text = userProfile?.email
-                name?.text = userProfile?.name
-            }
-        })
-        */
-/*
-        userRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val value = dataSnapshot.getValue<String>()
-                Log.d(TAG, "Value is: $value")
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-        })
-*/
         return view
     }
 
